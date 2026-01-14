8f76d791e272a1d1e75b752ea30c17510879beeb
felix <felix28004@gmail.com>
2026-01-14 19:33:00 +0100

    otro guardado

--- js/index.js
+++ js/index.js
@@ -9,30 +9,30 @@
 
 
 //globalThis.console.log("This works everywhere!");
-console.log("Operaciones Aritmeticas:");
+// console.log("Operaciones Aritmeticas:");
 
 
 
-import { suma, resta, multiplicacion, division } from '../operaciones-aritmeticas.mjs';
+// import { suma, resta, multiplicacion, division } from '../operaciones-aritmeticas.mjs';
 
-console.log("Suma:", suma(5, 3));
+// console.log("Suma:", suma(5, 3));
 
-console.log("Resta:", resta(5, 3));
+// console.log("Resta:", resta(5, 3));
 
-console.log("Multiplicacion:", multiplicacion(5, 3));
+// console.log("Multiplicacion:", multiplicacion(5, 3));
 
-console.log("Division:", division(6, 3));
+// console.log("Division:", division(6, 3));
 
 
 
 // Get HTML elements by ID
 
-import { suma, resta, multiplicacion, division } from '../operaciones-aritmeticas.mjs';
+import { suma, resta, multiplicacion, division } from './operaciones-aritmeticas.mjs';
 
 //seleccionar los elementos del DOM
 // zona numeros aleatorios
-const numero1 = document.getElementById('numeroIzq');
-const numero2 = document.getElementById('numeroDerech');
+const numeroIzq = document.getElementById('numeroIzq');
+const numeroDerech = document.getElementById('numeroDerech');
 
 // zona boton numeros nuevos
 const nuevoBtn = document.getElementById('btnNuevosNumeros');
@@ -45,26 +45,57 @@ const btnSumar = document.getElementById('btnSumar');
 const btnRestar = document.getElementById('btnRestar');
 
 // zona botones y resultado multiplicar y dividir
-const btnMultiplicar = document.getElementById('resultadoMult');
-const resultadoMult = document.getElementById('resultadoDiv');
-const resultadoDiv = document.getElementById('btnMult');
-const btnDividir = document.getElementById('btnDiv');
+const resultadoMult = document.getElementById('resultadoMult');
+const resultadoDiv = document.getElementById('resultadoDiv');
+const btnMult = document.getElementById('btnMult');
+const btnDiv = document.getElementById('btnDiv');
 
 
 
-// Add event listeners
-sumaBtn.addEventListener('click', () => {
-    resultado.textContent = suma(5, 3);
+// funcion de un numero aleatorio entre el 0 y el 99
+function numeroAleatorio() {
+    return Math.floor(Math.random() * 100);
+}
+
+//Cuando voy a necesitar dos numeros aleatorios, cada uno en una constante, los voy a necesiar al cargar la pagina y al pulsar el boton de nuevos numeros
+
+let aleatorioIzq = numeroAleatorio();
+let aleatorioDerech = numeroAleatorio();
+numeroIzq.textContent = numeroAleatorio;
+numeroDerech.textContent = numeroAleatorio;
+
+//evento para el boto de nuevos numeros
+btnNuevosNumeros.addEventListener('click', () => {
+    aleatorioIzq = numeroAleatorio();
+    aleatorioDerech = numeroAleatorio();
+    numeroIzq.textContent = aleatorioIzq;
+    numeroDerech.textContent = aleatorioDerech;
+
+    resultadoSuma.textContent = '';
+    resultadoResta.textContent = '';
+    resultadoMult.textContent = '';
+    resultadoDiv.textContent = '';
 });
 
-restaBtn.addEventListener('click', () => {
-    resultado.textContent = resta(5, 3);
+// evento botones operaciones
+btnSumar.addEventListener('click', () => {
+    const resultado = suma(aleatorioIzq, aleatorioDerech);
+    resultadoSuma.textContent = `Resultado: ${resultado}`;
 });
 
-multiBtn.addEventListener('click', () => {
-    resultado.textContent = multiplicacion(5, 3);
+btnRestar.addEventListener('click', () => {
+    const resultado = resta(aleatorioIzq, aleatorioDerech);
+    resultadoResta.textContent = `Resultado: ${resultado}`;
 });
 
-divBtn.addEventListener('click', () => {
-    resultado.textContent = division(6, 3);
+btnMult.addEventListener('click', () => {
+    const resultado = multiplicacion(aleatorioIzq, aleatorioDerech);
+    resultadoMult.textContent = `Resultado: ${resultado}`;
 });
+
+btnDiv.addEventListener('click', () => {
+    const resultado = division(aleatorioIzq, aleatorioDerech);
+    resultadoDiv.textContent = `Resultado: ${resultado}`;
+});
+
+
